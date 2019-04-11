31524d0d27fe83bf4c874510f6baa90191018205
Hagafny <ronhagafny20@gmail.com>
2019-04-11 16:29:24 +0300

    Added more features to the api

--- api/service.js
+++ api/service.js
@@ -1,15 +1,14 @@
+const request = require("request");
+
 const token =
   "[REDACTED]";
-
-const request = require("request");
 
 const sendMessage = (phone, message) => {
   var options = {
     method: "POST",
     url: "https://api.wassenger.com/v1/messages",
     headers: {
-      token:
-        "[REDACTED]",
+      token: token,
       "content-type": "application/json"
     },
     body: {
@@ -25,8 +24,25 @@ const sendMessage = (phone, message) => {
   });
 };
 
+const getMessages = (phone, cb) => {
+  var options = {
+    method: "GET",
+    url: "https://api.wassenger.com/v1/messages",
+    qs: { size: "10", status: "sent", phone: phone },
+    headers: { token: token }
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+    cb(body);
+  });
+};
+
 const service = {
-  sendMessage
+  sendMessage,
+  getMessages
 };
 
 module.exports = service;
